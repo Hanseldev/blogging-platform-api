869491a165ef8149355bc9618613d8750f1b3c06
Hansel <hauwmgames@gmail.com>
2026-07-13 23:27:22 +0100

    feat: Add PUT and DELETE /posts/:id

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { createPostSchema } from "../logic/postValidation.js";
 import { prisma } from "../lib/prisma.js";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const article = req.body;
 		const result = createPostSchema.safeParse(article);
@@ -16,15 +16,65 @@ router.post("/", async (req: Request, res: Response) => {
 		});
 		res.status(201).json(newPost);
 	} catch (err) {
-		console.error(err);
-		return res.status(500).json({ error: "Internal server error" });
+		return next(err);
 	}
 });
 
-router.get("/", (req: Request, res: Response) => {
-	res.json({ message: "get all posts - not implemented yet" });
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const articles = await prisma.post.findMany();
+		return res.status(200).json(articles);
+	} catch (err) {
+		return next(err);
+	}
 });
 
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const id = Number(req.params.id);
+		const article = await prisma.post.findUnique({ where: { id: id } });
+		if (!article) return res.status(404).json({ message: "Article not found" });
+		return res.status(200).json(article);
+	} catch (err) {
+		return next(err);
+	}
+});
 
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const id = Number(req.params.id);
+		const updatedArticle = req.body;
+		const result = createPostSchema.safeParse(updatedArticle);
+
+		if (!result.success)
+			return res.status(400).json({ message: "Bad Request" });
+		if (!(await prisma.post.findUnique({ where: { id: id } })))
+			return res.status(404).json({ message: "Article not found" });
+
+		const updatedPost = await prisma.post.update({
+			where: { id: id },
+			data: result.data,
+		});
+		return res.status(200).json(updatedPost);
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.delete(
+	"/:id",
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const id = Number(req.params.id);
+			if (!(await prisma.post.findUnique({ where: { id: id } })))
+				return res.status(404).json({ message: "Article not found" });
+
+			await prisma.post.delete({ where: { id: id } });
+			return res.sendStatus(204);
+		} catch (err) {
+			return next(err);
+		}
+	},
+);
 
 export default router;
